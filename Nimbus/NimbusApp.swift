8c327071e75cd98f569bf164679bdc3216f08ca1
Alastair Longmuir <alastairlinwood@gmail.com>
2024-10-01 15:58:18 +0200

    Initial Commit

---
+++ Nimbus/NimbusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbusApp.swift
+//  Nimbus
+//
+//  Created by Alastair Longmuir on 01.10.24.
+//
+
+import SwiftUI
+
+@main
+struct NimbusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
